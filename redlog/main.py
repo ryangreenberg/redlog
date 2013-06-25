be2a70037d10ee87b3b7822a60ad362b87b39867
Nick Doty <npdoty@ischool.berkeley.edu>
2013-06-24 19:09:01 -0700

    route with username variable

--- redlog/main.py
+++ redlog/main.py
@@ -3,8 +3,16 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+      self.response.write('Hello world!')
+
+class ApiHandler(webapp2.RequestHandler):
+    def get(self, username):
+      self.response.write('GET %s' % username)
+    
+    def post(self, username):
+      self.response.write('POST %s' % username)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+  ('/', MainHandler),
+  webapp2.Route(r'/v1/<username>/read', handler=ApiHandler, name='api-read')
 ], debug=True)
